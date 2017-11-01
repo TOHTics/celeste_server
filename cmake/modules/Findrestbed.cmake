5476377041a896880e37ac4d89205c1f14f938c3
Carlos Brito <carlos.brito524@gmail.com>
2017-10-31 20:39:17 -0600

    Update Findrestbed.cmake

--- cmake/modules/Findrestbed.cmake
+++ cmake/modules/Findrestbed.cmake
@@ -8,13 +8,13 @@ find_path(restbed_INCLUDE restbed
           "/opt/local/include"
           )
 
-find_library(restbed_LIBRARY restbed 
-              HINTS
-              "${PROJECT_SOURCE_DIR}"
-              "${PROJECT_SOURCE_DIR}/build/dependency/restbed"
-              "/usr/local/lib"
-              "/opt/local/lib"
-              )
+# find_library(restbed_LIBRARY restbed 
+#              HINTS
+#              "${PROJECT_SOURCE_DIR}"
+#              "${PROJECT_SOURCE_DIR}/build/dependency/restbed"
+#              "/usr/local/lib"
+#              "/opt/local/lib"
+#              )
 
 # ---- Verify we found the dependency ----------
 if (restbed_INCLUDE)
@@ -23,13 +23,13 @@ else()
     message(FATAL_ERROR "${red}Failed to locate restbed include files.${reset}")    
 endif()
 
-if (restbed_LIBRARY)
-    message(STATUS "${green}Found restbed library at: ${restbed_LIBRARY}${reset}")
-else()
-    message(WARNING "${red}Failed to locate restbed library.${reset}")  
-endif()
+# if (restbed_LIBRARY)
+#    message(STATUS "${green}Found restbed library at: ${restbed_LIBRARY}${reset}")
+# else()
+#     message(WARNING "${red}Failed to locate restbed library.${reset}")  
+# endif()
 
 
-if (restbed_INCLUDE AND restbed_LIBRARY)
+if (restbed_INCLUDE)
     set(RESTBED_FOUND TRUE)
 endif()
