e2374b35a163ac3252c210fff5572cc593bb5a86
Carlos Brito <carlosdotb@gmail.com>
2017-12-04 11:06:28 -0600

    Encapsulate Device

--- server/srv/service/device/device.cpp
+++ server/srv/service/device/device.cpp
@@ -5,234 +5,236 @@
  * 
  * @file
  */
-#include <iostream>
-#include "srv/db/db.hpp"
-#include "srv/service/common.hpp"
+#include "device1.hpp"
 #include "srv/service/error.hpp"
-#include "device.hpp"
+#include "srv/service/common.hpp"
+#include <iostream>
 
 using namespace std;
-
-using json = nlohmann::json;
 
 namespace celeste
 {
 namespace resource
-{
-
-    void get_device(const shared_ptr<restbed::Session> session,
-                    const shared_ptr<mysqlx::Session> dbSession)
+{   
+    // --- CLASS DEFINITIONS ---------
+    Devices<nlohmann::json>::Devices(const mysqlx::SessionSettings& dbSettings)
+        :   dbSession(dbSettings),
+            celesteDB(dbSession.getSchema("Celeste")),
+            deviceTable(celesteDB.getTable("Device"))
     {
-        const auto request = session->get_request();
-        size_t content_length = (size_t) request->get_header("Content-Length", 0);
-
-        json data;
-        session->fetch(content_length,
-        [ request, dbSession, &data ] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes)
-        {
-            string body;
-            bytes2string(bytes, body);
-
-            data = json::parse(body);
-        });
-
-        auto sch = dbSession->getSchema("Celeste");
-        auto table = sch.getTable("Device");
-
-        try
-        {
-            auto res = table.
-                select("*").
-                where("id = :DeviceId").
-                bind(ValueMap{{"DeviceId", data["DeviceId"].get<int>()}}).
-                execute();
-
-                string rb = json(move(res)).dump();
-                session->close(restbed::OK,
-                               rb,
-                               {
-                                   { "Content-Length", to_string(rb.size()) },
-                                   { "Connection",     "close" }
-                               });
-
-        } catch (const mysqlx::Error& e)
-        {
-            handle_error(restbed::INTERNAL_SERVER_ERROR,
-                         "Could not get Device " + to_string(data["DeviceId"].get<int>()),
-                         session
-                         );
-        }
+        set_path("/devices");
+        set_method_handler("GET", [this] (const std::shared_ptr<restbed::Session> session) {GET(session);});
+        set_method_handler("POST",   [this] (const std::shared_ptr<restbed::Session> session) {POST(session);});
+        set_method_handler("DELETE", [this] (const std::shared_ptr<restbed::Session> session) {DELETE(session);});
     }
 
-    void save_device(const shared_ptr<restbed::Session> session,
-                     const shared_ptr<mysqlx::Session> dbSession)
+    Device Devices<nlohmann::json>::get(int deviceId)
     {
-        const auto request = session->get_request();
-        size_t content_length = (size_t) request->get_header("Content-Length", 0);
+        auto res = 
+            deviceTable.
+            select("*").
+            where("id = :DeviceId").
+            bind(ValueMap{{"DeviceId", deviceId}}).
+            execute();
 
-        json data;
-        session->fetch(content_length,
-        [ request, dbSession, &data] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes)
-        {
-            // Get body
-            string body;
-            bytes2string(bytes, body);
+        mysqlx::SerializableRow row = res.fetchOne();
+        json_type j = row.as<Device>();
+        cout << j << "\n";
+        return row.as<Device>();
+    }
 
-            data = json::parse(body);
+    boost::optional<int> Devices<nlohmann::json>::insert(const value_type& device, bool autogen)
+    {
+        Device dtmp = device;
+        if (autogen)
+            dtmp.DeviceId = dbSession.sql("SELECT IFNULL(MAX(id) + 1, 0) FROM Device").execute().fetchOne().get(0);
 
-        });
+        mysqlx::SerializableRow row;
+        row.from(dtmp);
 
-        auto sch = dbSession->getSchema("Celeste");
-        auto table = sch.getTable("Device");
+        mysqlx::Row rtmp = row;
 
-        // should we autogen the key?
-        bool autogen;
-        if (data["autogen"].is_null())
-            autogen = false;
-        else 
-            autogen = data["autogen"].get<bool>();
+        deviceTable.
+        insert("id", "Client_id", "man", "mod", "sn").
+        values(rtmp).
+        execute();
 
         if (autogen)
-        {
-            int autogen_id = dbSession->sql("SELECT IFNULL(MAX(id) + 1, 0) FROM Device").execute().fetchOne().get(0);
-
-            table.
-            insert("id", "Client_id", "man", "mod", "sn").
-            values(autogen_id,
-                   data["ClientId"],
-                   data["man"].get<string>().c_str(),
-                   data["mod"].get<string>().c_str(),
-                   data["sn"].get<string>().c_str()).
-            execute();
-
-            json response;
-            response["DeviceId"] = autogen_id;
-            string rb = response.dump();
-            session->close(restbed::OK,
-                               rb,
-                               {
-                                   { "Content-Length", to_string(rb.size()) },
-                                   { "Connection",     "close" }
-                               });
-        } else
-        {
-            table.insert("id", "Client_id", "man", "mod", "sn").
-            values(data["DeviceId"].get<int>(),
-                   data["ClientId"],
-                   data["man"].get<string>().c_str(),
-                   data["mod"].get<string>().c_str(),
-                   data["sn"].get<string>().c_str()).
-            execute();
-            session->close(restbed::OK);
-        }
+            return dtmp.DeviceId;
+        else
+            return boost::none;
     }
 
-    void delete_device(const shared_ptr<restbed::Session> session,
-                       const shared_ptr<mysqlx::Session> dbSession)
+    void Devices<nlohmann::json>::remove(int deviceId)
     {
+        deviceTable.
+        remove().
+        where("id = :DeviceId").
+        bind(ValueMap{{"DeviceId", deviceId}}).
+        execute();
+    }
+
+    void Devices<nlohmann::json>::GET(const std::shared_ptr<restbed::Session> session)
+    {   
+        // get request
         const auto request = session->get_request();
+
+        // get headers
         size_t content_length = (size_t) request->get_header("Content-Length", 0);
 
-        json data;
-        session->fetch(content_length,
-        [ request, dbSession, &data] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes)
-        {
-            // Get body
-            string body;
-            bytes2string(bytes, body);
+        // fetch data to access later
+        session->fetch(content_length, [] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes) {});
 
-            data = json::parse(body);
-        });
+        // get json from request
+        json_type data = get_json<json_type>(*request);
 
-        int device_id = data["DeviceId"];
-        try
-        {
-            auto sch = dbSession->getSchema("Celeste");
-            auto table = sch.getTable("Device");
+        // validate data
+        if (data["DeviceId"].is_null())
+            throw 400;
 
-            table.
-            remove().
-            where("id = :DeviceId").
-            bind(ValueMap{{"DeviceId", device_id}}).
-            execute();
+        // get device from db
+        json_type response = this->get(data["DeviceId"]);
 
-            session->close(restbed::OK);
-        }
-        catch (const mysqlx::Error& e)
-        {
-            handle_error(restbed::INTERNAL_SERVER_ERROR,
-                         "Failed to delete device with id " + to_string(device_id),
-                         session);
-        }
+        // close
+        session->close(restbed::OK,
+                       response.dump(),
+                       {
+                            { "Content-Length", to_string(response.dump().size()) },
+                            { "Connection",     "close" }
+                       });
     }
 
-    void add_model(const shared_ptr<restbed::Session> session,
-                   const shared_ptr<mysqlx::Session> dbSession)
+    void Devices<nlohmann::json>::POST(const std::shared_ptr<restbed::Session> session)
     {
+        // get request
         const auto request = session->get_request();
+
+        // get headers
         size_t content_length = (size_t) request->get_header("Content-Length", 0);
 
-        // fetch data
-        json data;
-        session->fetch(content_length,
-        [ request, dbSession, &data] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes)
+        // fetch data to access later
+        session->fetch(content_length, [] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes) {});
+
+        // get json from request
+        json_type data = get_json<json_type>(*request);
+
+        // validate data
+        bool autogen = true;
+        if (data["autogen"].is_null())
+            autogen = false;
+        else
+            autogen = data["autogen"].get<bool>();
+
+        if (data["DeviceId"].is_null())
         {
-            string body;
-            bytes2string(bytes, body);
-
-            data = json::parse(body);
-        });
-
-        int device_id = data["DeviceId"];
-        string model_id = data["ModelId"];
-
-        try
-        {
-            auto sch = dbSession->getSchema("Celeste");
-            auto table = sch.getTable("Device_Model");
-
-            auto res = table.select("MAX(idx)").
-                where("Device_id = :DeviceId AND Model_id = :ModelId").
-                bind(ValueMap{{"DeviceId", device_id}, {"ModelId", model_id.c_str()}}).execute();
-
-            int max_idx = 0;
-            if (res.count() > 0)
-                max_idx = res.fetchOne().get(0);
-
-            table.
-            insert("idx", "Device_id", "Model_id").
-            values(max_idx + 1,device_id, model_id.c_str()).
-            execute();
-            
-            json rp;
-            rp["x"] = max_idx + 1;
-
-            session->close(restbed::OK, rp.dump());
-        } catch (const mysqlx::Error& e)
-        {
-            handle_error(restbed::INTERNAL_SERVER_ERROR,
-                         "Could not add Model " + model_id + " to Device " + to_string(device_id),
-                         session);
+            data["DeviceId"] = -1;
+            if (!autogen)
+                throw 400;
         }
+
+        if (data["man"].is_null())
+            throw 400;
+
+        if (data["mod"].is_null())
+            throw 400;
+
+        if (data["sn"].is_null())
+            throw 400;
+
+        data["ClientId"] = 23;
+
+        // insert device and get id
+        // in case autogen was not set, will return null
+        Device device = data.get<Device>();
+        auto autogen_id = this->insert(data.get<Device>(), autogen);
+        json_type response{{"x", autogen_id}};
+
+        // close
+        session->close(restbed::OK,
+                       response.dump(),
+                       {
+                            { "Content-Length", to_string(response.dump().size()) },
+                            { "Connection",     "close" }
+                       });
     }
 
-    shared_ptr<restbed::Resource> make_device(const shared_ptr<mysqlx::Session> dbSession)
+    void Devices<nlohmann::json>::DELETE(const std::shared_ptr<restbed::Session> session)
     {
-        auto resource = make_shared<restbed::Resource>();
-        resource->set_path("/devices");
-        resource->set_method_handler("GET", bind(get_device, placeholders::_1, dbSession));
-        resource->set_method_handler("POST", bind(save_device, placeholders::_1, dbSession));
-        resource->set_method_handler("DELETE", bind(delete_device, placeholders::_1, dbSession));
-        return move(resource);
-    }
+        // get request
+        const auto request = session->get_request();
 
-    shared_ptr<restbed::Resource> make_add_model(const shared_ptr<mysqlx::Session> dbSession)
-    {
-        auto resource = make_shared<restbed::Resource>();
-        resource->set_path("/devices/add_model");
+        // get headers
+        size_t content_length = (size_t) request->get_header("Content-Length", 0);
 
-        resource->set_method_handler("POST", bind(add_model, placeholders::_1, dbSession));
-        return move(resource);
+        // fetch data to access later
+        session->fetch(content_length, [] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes) {});
+
+        // get json from request
+        json_type data = get_json<json_type>(*request);
+
+        // validate data
+        if (data["DeviceId"].is_null())
+            throw 400;
+
+        // remove device from DB.
+        this->remove(data["DeviceId"]);
+
+        // close
+        session->close(restbed::OK);
     }
 }
+}
+
+// --- SERIALIZERS -------------------
+namespace mysqlx
+{
+    using namespace celeste::resource;
+
+    void row_serializer<Device>::to_row (SerializableRow& row, const celeste::resource::Device& device)
+    {
+        row.set(0, device.DeviceId);
+        row.set(1, EnhancedValue(device.ClientId));
+        row.set(2, device.man.c_str());
+        row.set(3, device.mod.c_str());
+        row.set(4, device.sn.c_str());
+    }
+
+    void row_serializer<Device>::from_row (const SerializableRow& row, Device& device)
+    {
+        SerializableRow tmp = row; // row.get() is not marked const, hence we need this tmp
+        device = Device {
+            .DeviceId   = tmp.get(0),
+            .ClientId   = tmp.get(1),
+            .man        = static_cast<string>(tmp.get(2)),
+            .mod        = static_cast<string>(tmp.get(3)),
+            .sn         = static_cast<string>(tmp.get(4))
+        };
+    }
+}
+
+namespace nlohmann
+{
+    using namespace celeste::resource;
+
+    void adl_serializer<Device>::to_json(json& j, const Device& device)
+    {
+        j = json {
+            {"DeviceId",    device.DeviceId},
+            {"ClientId",    device.ClientId},
+            {"man",         device.man},
+            {"mod",         device.mod},
+            {"sn",          device.sn}
+        };
+    }
+
+    void adl_serializer<Device>::from_json(const json& j, Device& device)
+    {
+        device = Device {
+            .DeviceId   = j.at("DeviceId"),
+            .ClientId   = j.at("ClientId"),
+            .man        = j.at("man"),
+            .mod        = j.at("mod"),
+            .sn         = j.at("sn")
+        };
+    }
 }
