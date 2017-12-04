b480dffef5f21bb808990315f177c966c3ff40c1
Carlos Brito <carlosdotb@gmail.com>
2017-12-04 15:02:24 -0600

    Encapsulate Model

--- server/srv/service/model/model.cpp
+++ server/srv/service/model/model.cpp
@@ -5,150 +5,186 @@
  * 
  * @file
  */
-#include <iostream>
-#include "srv/db/db.hpp"
-#include "srv/service/common.hpp"
 #include "model.hpp"
+#include "srv/service/error.hpp"
+#include "srv/service/common.hpp"
 
 using namespace std;
-
-using json = nlohmann::json;
 
 namespace celeste
 {
 namespace resource
-{
-
-    void get_model(const std::shared_ptr<restbed::Session> session,
-                   const std::shared_ptr<mysqlx::Session> dbSession)
+{   
+    // --- CLASS DEFINITIONS ---------
+    Models<nlohmann::json>::Models(const mysqlx::SessionSettings& dbSettings)
+        :   dbSession(dbSettings),
+            celesteDB(dbSession.getSchema("Celeste")),
+            modelTable(celesteDB.getTable("Model"))
     {
-        const auto request = session->get_request();
-        size_t content_length = (size_t) request->get_header("Content-Length", 0);
-
-        session->fetch(content_length,
-        [ request, dbSession ] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes)
-        {
-            string body;
-            bytes2string(bytes, body);
-
-            json data = json::parse(body);
-
-            auto sch = dbSession->getSchema("Celeste");
-            auto table = sch.getTable("Model");
-
-            try
-            {
-                auto res = table.
-                select("*").
-                where("id = :ModelId").
-                bind(ValueMap{{"ModelId", data["ModelId"].get<string>().c_str()}}).
-                execute();
-
-                string rb = json(move(res))[0].dump();
-                session->close(restbed::OK,
-                               rb,
-                               {
-                                   { "Content-Length", to_string(rb.size()) },
-                                   { "Connection",     "close" }
-                               });
-            } catch (const mysqlx::Error& e)
-            {
-                handle_error(restbed::INTERNAL_SERVER_ERROR,
-                             "Could not get Device " + data["ModelId"].get<string>(),
-                             session);
-            } catch (const json::exception& e)
-            {
-                handle_error(restbed::BAD_REQUEST,
-                             "",
-                             session);
-            }
-        });
-
+        set_path("/Models");
+        set_method_handler("GET", [this] (const std::shared_ptr<restbed::Session> session) {GET(session);});
+        set_method_handler("POST",   [this] (const std::shared_ptr<restbed::Session> session) {POST(session);});
+        set_method_handler("DELETE", [this] (const std::shared_ptr<restbed::Session> session) {DELETE(session);});
     }
 
-    void save_model(const shared_ptr<restbed::Session> session,
-                     const shared_ptr<mysqlx::Session> dbSession)
+    Model Models<nlohmann::json>::get(std::string modelId)
     {
-        const auto request = session->get_request();
-        size_t content_length = (size_t) request->get_header("Content-Length", 0);
-
-        session->fetch(content_length,
-        [ request, dbSession ] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes)
-        {
-            string body;
-            bytes2string(bytes, body);
-
-            json data = json::parse(body);
-
-            try
-            {
-                auto sch = dbSession->getSchema("Celeste");
-                auto table = sch.getTable("Model");
-
-                table.
-                insert("id", "ns").
-                values(data["ModelId"].get<string>().c_str(),
-                       data["ns"].get<string>().c_str()).
-                execute();
-
-                session->close(restbed::OK);
-            }
-            catch (const mysqlx::Error& e)
-            {
-                handle_error(restbed::INTERNAL_SERVER_ERROR,
-                             "Could not save Model", session);
-            }
-        });
-    }
-
-    void delete_model(const std::shared_ptr<restbed::Session> session,
-                       const std::shared_ptr<mysqlx::Session> dbSession)
-    {
-        const auto request = session->get_request();
-        size_t content_length = (size_t) request->get_header("Content-Length", 0);
-
-        json data;
-        session->fetch(content_length,
-        [ request, dbSession, &data] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes)
-        {
-            // Get body
-            string body;
-            bytes2string(bytes, body);
-
-            data = json::parse(body);
-        });
-
-        int model_id = data["ModelId"];
-        try
-        {
-            auto sch = dbSession->getSchema("Celeste");
-            auto table = sch.getTable("Model");
-
-            table.
-            remove().
+        auto res = 
+            modelTable.
+            select("*").
             where("id = :ModelId").
-            bind(ValueMap{{"ModelId", model_id}}).
+            bind(ValueMap{{"ModelId", modelId.c_str()}}).
             execute();
 
-            session->close(restbed::OK);
-        }
-        catch (const mysqlx::Error& e)
-        {
-            handle_error(restbed::INTERNAL_SERVER_ERROR,
-                         "Failed to delete model with id " + to_string(model_id),
-                         session);
-        }
+        mysqlx::SerializableRow row = res.fetchOne();
+        json_type j = row.as<Model>();
+        return row.as<Model>();
     }
 
-    shared_ptr<restbed::Resource> make_model(const shared_ptr<mysqlx::Session> dbSession)
+    void Models<nlohmann::json>::insert(const value_type& model)
     {
-        auto resource = make_shared<restbed::Resource>();
-        resource->set_path("models/");
-        resource->set_method_handler("POST", bind(save_model, placeholders::_1, dbSession));
-        resource->set_method_handler("GET", bind(get_model, placeholders::_1, dbSession));
-        resource->set_method_handler("DELETE", bind(delete_model, placeholders::_1, dbSession));
+        mysqlx::SerializableRow row;
+        row.from(model);
 
-        return move(resource);
+        mysqlx::Row tmp = row;
+
+        modelTable.
+        insert("id", "ns").
+        values(tmp).
+        execute();
+    }
+
+    void Models<nlohmann::json>::remove(std::string modelId)
+    {
+        modelTable.
+        remove().
+        where("id = :ModelId").
+        bind(ValueMap{{"ModelId", modelId.c_str()}}).
+        execute();
+    }
+
+    void Models<nlohmann::json>::GET(const std::shared_ptr<restbed::Session> session)
+    {   
+        // get request
+        const auto request = session->get_request();
+
+        // get headers
+        size_t content_length = (size_t) request->get_header("Content-Length", 0);
+
+        // fetch data to access later
+        session->fetch(content_length, [] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes) {});
+
+        // get json from request
+        json_type data = get_json<json_type>(*request);
+
+        // validate data
+        if (data["ModelId"].is_null())
+            throw 400;
+
+        // get model from db
+        json_type response = this->get(data["ModelId"]);
+
+        // close
+        session->close(restbed::OK,
+                       response.dump(),
+                       {
+                            { "Content-Length", to_string(response.dump().size()) },
+                            { "Connection",     "close" }
+                       });
+    }
+
+    void Models<nlohmann::json>::POST(const std::shared_ptr<restbed::Session> session)
+    {
+        // get request
+        const auto request = session->get_request();
+
+        // get headers
+        size_t content_length = (size_t) request->get_header("Content-Length", 0);
+
+        // fetch data to access later
+        session->fetch(content_length, [] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes) {});
+
+        // get json from request
+        json_type data = get_json<json_type>(*request);
+
+        // validate data
+        if (data["ModelId"].is_null())
+                throw 400;
+
+        if (data["ns"].is_null())
+            data["ns"] = nullptr;
+
+        this->insert(data.get<Model>());
+
+        // close
+        session->close(restbed::OK);
+    }
+
+    void Models<nlohmann::json>::DELETE(const std::shared_ptr<restbed::Session> session)
+    {
+        // get request
+        const auto request = session->get_request();
+
+        // get headers
+        size_t content_length = (size_t) request->get_header("Content-Length", 0);
+
+        // fetch data to access later
+        session->fetch(content_length, [] (const shared_ptr<restbed::Session> session, const restbed::Bytes &bytes) {});
+
+        // get json from request
+        json_type data = get_json<json_type>(*request);
+
+        // validate data
+        if (data["ModelId"].is_null())
+            throw 400;
+
+        // remove model from DB.
+        this->remove(data["ModelId"]);
+
+        // close
+        session->close(restbed::OK);
     }
 }
+}
+
+// --- SERIALIZERS -------------------
+namespace mysqlx
+{
+    using Model = celeste::resource::Model;
+
+    void row_serializer<Model>::to_row (SerializableRow& row, const celeste::resource::Model& obj)
+    {
+        row.set(0, EnhancedValue{obj.ModelId});
+        row.set(1, EnhancedValue{obj.ns});
+    }
+
+    void row_serializer<Model>::from_row (const SerializableRow& row, Model& obj)
+    {
+        SerializableRow tmp = row; // row.get() is not marked const, hence we need this tmp
+        obj = Model {
+            .ModelId = tmp.get(0).get<std::string>(),
+            .ns = tmp.get(1)
+        };
+    }
+}
+
+namespace nlohmann
+{
+    using namespace celeste::resource;
+
+    void adl_serializer<Model>::to_json(json& j, const Model& obj)
+    {
+        j = json {
+            {"ModelId", obj.ModelId},
+            {"ns", obj.ns}
+        };
+    }
+
+    void adl_serializer<Model>::from_json(const json& j, Model& obj)
+    {
+        obj = Model {
+            .ModelId = j.at("ModelId"),
+            .ns = j.at("ns")
+        };
+    }
 }
--- server/srv/service/model/model.hpp
+++ server/srv/service/model/model.hpp
@@ -1,3 +1,5 @@
+
+
 /**
  * * * * * * * * * * * * * * * * * * * *
  * Copyright (C) 7/Nov/17 Carlos Brito *
@@ -5,34 +7,127 @@
  * 
  * @file
  */
-
 #ifndef SERVER_RESOURCE_MODEL_HPP
 #define SERVER_RESOURCE_MODEL_HPP
 
 #include <string>
-#include <iostream>
 #include <memory>
 #include <mysql_devapi.h>
 #include <json.hpp>
 #include <restbed>
 #include <functional>
+#include <boost/optional.hpp>
+
+#include "srv/db/db.hpp"
 
 namespace celeste
 {   
 namespace resource
 {
-    void get_model(const std::shared_ptr<restbed::Session> session,
-                   const std::shared_ptr<mysqlx::Session> dbSession);
-    
-    void save_model(const std::shared_ptr<restbed::Session> session,
-                    const std::shared_ptr<mysqlx::Session> dbSession);
-    
-    void delete_model(const std::shared_ptr<restbed::Session> session,
-                     const std::shared_ptr<mysqlx::Session> dbSession);
+    /**
+     * @brief      Represents a Model on the DB.
+     */
+    struct Model
+    {
+        typedef std::string id_type;
 
-    std::shared_ptr<restbed::Resource> make_model(const std::shared_ptr<mysqlx::Session> dbSession);
+        Model() = default;
+
+        id_type                         ModelId;
+        boost::optional<std::string>    ns;
+    };
+
+    /**
+     * @brief      Models resource class.
+     *
+     * @tparam     Json  Json type to use
+     */
+    template <class Json>
+    class Models : public restbed::Resource
+    {
+    };
+
+    /**
+     * @brief      Specialization for `nlohmann::json`.
+     */
+    template <>
+    class Models<nlohmann::json> : public restbed::Resource
+    {
+    public:
+        // --- Typedefs --------------
+        typedef Model               value_type;
+        typedef nlohmann::json      json_type;
+
+        // --- Constructors ----------
+
+        /**
+         * @brief      Creates a resource where you can access the Models
+         * on the DB.
+         *
+         * @param[in]  dbSettings  DB settings for connection.
+         */
+        Models(const mysqlx::SessionSettings& dbSettings);
+
+        // --- Public methods --------
+
+        /**
+         * @brief      Gets a Model from the DB.
+         *
+         * @param[in]  modelId  The Model identifier.
+         *
+         * @return     Fetched Model.
+         */
+        Model get(std::string modelId);
+
+        /**
+         * @brief      Inserts a Model into the DB.
+         *
+         * @param[in]  model   Model to be inserted.
+         */
+        void insert(const value_type& model);
+
+        /**
+         * @brief      Removes a Model from the DB.
+         *
+         * @param[in]  modelId  The Model identifier
+         */
+        void remove(std::string modelId);
+
+    private:
+        // --- Private methods -------
+        void GET(const std::shared_ptr<restbed::Session> session);
+        void POST(const std::shared_ptr<restbed::Session> session);
+        void DELETE(const std::shared_ptr<restbed::Session> session);
+
+        // --- Member attributes -----
+        mysqlx::Session     dbSession;
+        mysqlx::Schema      celesteDB;
+        mysqlx::Table       modelTable;
+    };
 }
 }
 
+// --- JSON SERIALIZATION ------------
+namespace nlohmann
+{
+    template <>
+    struct adl_serializer<celeste::resource::Model>
+    {
+        static void to_json(json& j, const celeste::resource::Model& obj);
+        static void from_json(const json& j, celeste::resource::Model& obj);
+    }; 
+}
+
+// --- SQL SERIALIZATION -------------
+namespace mysqlx
+{
+    template <>
+    struct row_serializer<celeste::resource::Model>
+    {
+        static void to_row (SerializableRow& row, const celeste::resource::Model& obj);
+        static void from_row (const SerializableRow& row, celeste::resource::Model& obj);
+
+    };
+}
 
 #endif
